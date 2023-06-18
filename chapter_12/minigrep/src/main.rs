0dde040099ca7f9e9c42a1301a408742d2f8fccf
nazmulcuet11 <nazmulcuet11@gmail.com>
2023-06-18 16:42:44 +0700

    implement minigrep

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use minigrep;
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}.\nargs: {:#?}", err, args);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(&config) {
+        eprintln!("App error: {e}");
+        process::exit(1);
+    }
 }
